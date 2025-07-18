e89cc241692539ab404a026941bb00720f991473
shyx32 <shyx32@qq.com>
2025-07-18 13:00:53 +0800

    Handle falsy values in stringifyQuery

--- test/unit/router-util.test.js
+++ test/unit/router-util.test.js
@@ -1,4 +1,5 @@
 const { resolvePath } = require('../../src/core/util');
+const { stringifyQuery } = require('../../src/core/router/util');
 
 // Suite
 // -----------------------------------------------------------------------------
@@ -22,6 +23,28 @@ describe('router/util', () => {
       const result = resolvePath('test/../hello.md');
 
       expect(result).toBe('/hello.md');
+    });
+  });
+
+  // stringifyQuery()
+  // ---------------------------------------------------------------------------
+  describe('stringifyQuery()', () => {
+    test('preserves case of query values', () => {
+      const result = stringifyQuery({ Foo: 'Bar' });
+
+      expect(result).toBe('?Foo=Bar');
+    });
+
+    test('ignores specified keys', () => {
+      const result = stringifyQuery({ foo: 'Bar', id: '123' }, ['id']);
+
+      expect(result).toBe('?foo=Bar');
+    });
+
+    test('handles falsy values', () => {
+      const result = stringifyQuery({ zero: 0, empty: '' });
+
+      expect(result).toBe('?zero=0&empty=');
     });
   });
 });
